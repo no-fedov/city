52703ce0efe5b28c2fb415a455aa35ec0ac06872
Alexandwere <[lazukov.ao@gmail.com]>
2024-10-20 19:30:14 +0300

    refactor: Пересмотр модели, создание пустой дефолт коллекции детей

--- human/src/main/java/org/javaacademy/Human.java
+++ human/src/main/java/org/javaacademy/Human.java
@@ -5,12 +5,11 @@ import lombok.experimental.FieldDefaults;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@ToString(onlyExplicitlyIncluded = true)
 @Getter
+@ToString(onlyExplicitlyIncluded = true)
 public class Human {
     @NonNull
     @ToString.Include
@@ -24,7 +23,7 @@ public class Human {
     final boolean isMale;
     Human father;
     Human mother;
-    Set<Human> children;
+    final Set<Human> children = new HashSet<>(){};
 
     @Builder
     public Human(@NonNull String firstName, @NonNull String middleName, @NonNull String lastName, boolean isMale) {
@@ -32,26 +31,6 @@ public class Human {
         this.middleName = StringUtils.capitalize(middleName.toLowerCase());
         this.lastName = StringUtils.capitalize(lastName.toLowerCase());
         this.isMale = isMale;
-    }
-
-    public void setParents(Human parent1, Human parent2) {
-        genderCheck(parent1, parent2);
-        if (parent1.isMale) {
-            this.father = parent1;
-            this.mother = parent2;
-            addChildToParents(father, mother,this);
-        } else {
-            this.father = parent2;
-            this.mother = parent1;
-            addChildToParents(this.father, this.mother,this);
-        }
-    }
-
-    public Set<Human> getChildren() {
-        if (children == null) {
-            throw new RuntimeException("У человека нет детей.");
-        }
-        return children;
     }
 
     public Human makeChild(String firstName, String middleName, String lastName, boolean isMale, Human parent2) {
@@ -66,25 +45,27 @@ public class Human {
         return newHuman;
     }
 
+    private void setParents(Human parent1, Human parent2) {
+        genderCheck(parent1, parent2);
+        if (parent1.isMale) {
+            this.father = parent1;
+            this.mother = parent2;
+        } else {
+            this.father = parent2;
+            this.mother = parent1;
+        }
+        addChildToParents(father, mother,this);
+    }
+
+    private static void addChildToParents(Human parent1, Human parent2, Human child) {
+        parent1.children.add(child);
+        parent2.children.add(child);
+    }
+
     public static void genderCheck(Human human1, Human human2) {
         if (human1.isMale == human2.isMale) {
             throw new RuntimeException("Родителями не могут быть однополые люди.");
         }
-    }
-
-    public static void addChildToParents(Human parent1, Human parent2, Human child) {
-        if (parent1.children != null) {
-            parent1.children.add(child);
-        } else {
-            parent1.children = new HashSet<>(List.of(child));
-        }
-
-        if (parent2.children != null) {
-            parent2.children.add(child);
-        } else {
-            parent2.children = new HashSet<>(List.of(child));
-        }
-
     }
 
     public String getFullName() {
