1aaf22993278efdf8ae073964271ba4bcd3909b9
Alexandwere <[lazukov.ao@gmail.com]>
2024-10-21 20:42:08 +0300

    refactor: убран Builder

--- human/src/main/java/org/javaacademy/Human.java
+++ human/src/main/java/org/javaacademy/Human.java
@@ -1,12 +1,17 @@
 package org.javaacademy;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
+
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.HashSet;
 import java.util.Set;
-import static org.javaacademy.HumanUtil.*;
+
+import static org.javaacademy.HumanUtil.genderOppositeCheck;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
@@ -15,34 +20,32 @@ public class Human {
     @NonNull
     @ToString.Include
     final String firstName;
+
     @NonNull
     @ToString.Include
     final String middleName;
+
     @NonNull
     @ToString.Include
     final String lastName;
+
     final boolean isMale;
     Human father;
     Human mother;
     final Set<Human> children = new HashSet<>(){};
 
-    @Builder
-    public Human(@NonNull String firstName, @NonNull String middleName, @NonNull String lastName, boolean isMale) {
+    public Human(String firstName, String middleName, String lastName, boolean isMale) {
         this.firstName = StringUtils.capitalize(firstName.toLowerCase());
         this.middleName = StringUtils.capitalize(middleName.toLowerCase());
         this.lastName = StringUtils.capitalize(lastName.toLowerCase());
         this.isMale = isMale;
     }
 
+    @NonNull
     public Human makeChild(String firstName, String middleName, String lastName, boolean isMale,
                            Human parent2) {
         genderOppositeCheck(this, parent2);
-        Human newHuman = Human.builder()
-                .firstName(firstName)
-                .middleName(middleName)
-                .lastName(lastName)
-                .isMale(isMale)
-                .build();
+        Human newHuman = new Human(firstName, middleName, lastName, isMale);
         newHuman.setParents(this, parent2);
         return newHuman;
     }
@@ -59,13 +62,12 @@ public class Human {
         addChildToParents(father, mother, this);
     }
 
-    @NonNull
     private static void addChildToParents(Human parent1, Human parent2, Human child) {
         parent1.children.add(child);
         parent2.children.add(child);
     }
 
     public String getFullName() {
-        return String.format("Имя Отчество Фамилия: %s %s %s", firstName, middleName, lastName);
+        return String.format("ФИО: %s %s %s", lastName, firstName, middleName);
     }
 }
