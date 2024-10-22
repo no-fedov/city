42cc16f0a6dd6a6ba00bad9384f2367bfbc2b511
Karina <[karinakios@mail.ru]>
2024-10-22 22:02:23 +0300

    feat: запушила для примера

--- profession/src/main/java/org/javaacademy/Programmer.java
+++ profession/src/main/java/org/javaacademy/Programmer.java
@@ -20,23 +20,42 @@ public class Programmer extends Employee {
         super(name, surname, patronymic, isMale);
     }
 
+//    public void setRate(BigDecimal rate) {
+//        if (checkRate(rate)) {
+//            throw new RuntimeException();
+//        } else {
+//            this.rate = rate;
+//        }
+//    }
+//
+//    private boolean checkRate(BigDecimal rate) {
+//        int flag = 0;
+//        flag = rate.min(MIN_RATE).equals(rate) ? flag += 1 : flag;
+//        flag = rate.max(MAX_RATE).equals(rate) ? flag += 1 : flag;
+//        if (flag > 0) {
+//            return false;
+//        }
+//        return true;
+//    }
     public void setRate(BigDecimal rate) {
-        if (checkRate(rate)) {
-            throw new RuntimeException();
-        } else {
+        Predicate<BigDecimal> check = (BigDecimal chRate) -> chRate.min(MIN_RATE).equals(chRate) || chRate.max(MAX_RATE).equals(chRate);
+
+        if(checkRate(check, rate)) {
             this.rate = rate;
+        }
+        else {
+            throw new RuntimeException();
+        }
+    }
+    private boolean checkRate(Predicate<BigDecimal> checking, BigDecimal rate) {
+        if(!checking.test(rate)) {
+            return false;
+        }
+        else {
+            return true;
         }
     }
 
-    private boolean checkRate(BigDecimal rate) {
-        int flag = 0;
-        flag = rate.min(MIN_RATE).equals(rate) ? flag += 1 : flag;
-        flag = rate.max(MAX_RATE).equals(rate) ? flag += 1 : flag;
-        if (flag > 0) {
-            return false;
-        }
-        return true;
-    }
 
     private void takeTask(Task receivedtask) {
         if (receivedtask.isSolved() == false) {
