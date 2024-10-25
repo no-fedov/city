a0fa16de0492c0bdaf75a7332afcaec349fd6bbc
Alexandwere <[lazukov.ao@gmail.com]>
2024-10-25 20:49:36 +0300

    feat: реализован метод weekWork

--- company/src/main/java/org/javaacademy/Company.java
+++ company/src/main/java/org/javaacademy/Company.java
@@ -10,59 +10,53 @@ import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
 
 import java.math.BigDecimal;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Company {
-    @NonNull
     final String companyName;
-
-    @NonNull
     Manager manager;
-
-    @NonNull
     final Set<Programmer> programmers;
+    MultiValuedMap<Employee, Task> doneTasks = new HashSetValuedHashMap<>();
+    Map<Employee, Duration> timeSheet = new HashMap<>();
+    BigDecimal expenses;
 
-    final MultiValuedMap<Employee, Task> doneTasks = new HashSetValuedHashMap<>();
-
-    final Map<Employee, Duration> timeSheet = new HashMap<>();
-
-    BigDecimal expenses = BigDecimal.ZERO;
-
-    public Company(@NonNull String companyName,
-                   @NonNull Manager manager,
-                   @NonNull Set<Programmer> programmers,
-                   @NonNull BigDecimal programmerRate) {
+    public Company(String companyName,
+                   Manager manager,
+                   Set<Programmer> programmers,
+                   BigDecimal expenses,
+                   BigDecimal programmerRate) {
         this.companyName = companyName;
         this.manager = manager;
         this.programmers = programmers;
-        setRateForProgrammers(programmers, programmerRate);
-    }
+        this.expenses = expenses;
 
-    public void payForWeekWork() {
-        timeSheet.forEach((employee, hours) -> {
-            BigDecimal salary = calculateSalary(hours, employee.getRate());
-            paySalaryForEmployee(employee, salary);
-            this.expenses = expenses.add(salary);
-        });
-        timeSheet.clear();
-    }
-
-    private void paySalaryForEmployee(Employee employee, BigDecimal salary) {
-        employee.receiveMoney(salary);
-    }
-
-    private BigDecimal calculateSalary(Duration hours, BigDecimal rate) {
-        return BigDecimal.valueOf(hours.toHours()).multiply(rate);
-    }
-
-    private void setRateForProgrammers(Set<Programmer> programmers, BigDecimal rate) {
         for (Programmer programmer : programmers) {
-            programmer.setRate(rate);
+            programmer.setRate(programmerRate);
         }
+    }
+
+    public void weekWork(@NonNull List<Task> weekTasks) {
+        ArrayList<Programmer> programmersToList = new ArrayList<>(programmers);
+        int numKeys = programmers.size();
+        int numValues = weekTasks.size();
+
+        for (int i = 0; i < numValues; i++) {
+            Programmer key = programmersToList.get(i % numKeys);
+            doneTasks.put(key, weekTasks.get(i));
+            System.out.printf("[%s] - сделана.", weekTasks.get(i).getSpecification());
+
+            timeSheet.put(key, (weekTasks.get(i).getHours().plus(timeSheet.get(key))));
+            timeSheet.put(manager, (weekTasks.get(i).getHours().plus(timeSheet.get(key))));
+//            у менеджера требуется умножение на коэффициент 0.1. Не знаю как сделать.
+//            Возможно стоит изменить duration на int и не париться. На работе никто минуты не считает,
+//            только часы
+        }
+    }
+
+    private static void tasksDescription(Task doneTask) {
+        System.out.printf("[%s] - сделана.", doneTask.getSpecification());
     }
 }
