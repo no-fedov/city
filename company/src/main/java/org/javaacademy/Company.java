b80e85d8965c06a143d327d1aaaf89e8e3ee8417
no_fedov <nefedov.evgeniy.d@yandex.ru>
2024-10-25 18:50:34 +0300

    refactor: убраны лишние поля из конструктора добавлены аннотации ломбок, инициализированы поля с коллекциями

--- company/src/main/java/org/javaacademy/Company.java
+++ company/src/main/java/org/javaacademy/Company.java
@@ -2,12 +2,15 @@ package org.javaacademy;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
 
 import java.math.BigDecimal;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -15,29 +18,34 @@ import java.util.Set;
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Company {
+    @NonNull
     final String companyName;
-    Manager manager;
-    final Set<Programmer> programmers;
-    final MultiValuedMap<Employee, Task> doneTasks;
-    final Map<Employee, Duration> timeSheet;
-    BigDecimal expenses;
 
-    public Company(String companyName,
-                   Manager manager,
-                   Set<Programmer> programmers,
-                   MultiValuedMap<Employee, Task> doneTasks,
-                   Map<Employee, Duration> timeSheet,
-                   BigDecimal expenses,
-                   BigDecimal programmerRate) {
+    @NonNull
+    Manager manager;
+
+    @NonNull
+    final Set<Programmer> programmers;
+
+    final MultiValuedMap<Employee, Task> doneTasks = new HashSetValuedHashMap<>();
+
+    final Map<Employee, Duration> timeSheet = new HashMap<>();
+
+    BigDecimal expenses = BigDecimal.ZERO;
+
+    public Company(@NonNull String companyName,
+                   @NonNull Manager manager,
+                   @NonNull Set<Programmer> programmers,
+                   @NonNull BigDecimal programmerRate) {
         this.companyName = companyName;
         this.manager = manager;
         this.programmers = programmers;
-        this.doneTasks = doneTasks;
-        this.timeSheet = timeSheet;
-        this.expenses = expenses;
+        setRateForProgrammers(programmers, programmerRate);
+    }
 
+    private void setRateForProgrammers(Set<Programmer> programmers, BigDecimal rate) {
         for (Programmer programmer : programmers) {
-            programmer.setRate(programmerRate);
+            programmer.setRate(rate);
         }
     }
 }
